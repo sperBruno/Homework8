6caacee1428fe984d24d96e75a6be0de6dae8a64
Bruno Luis Barrios Vargas <Bruno.Barrios@jalasoft.com>
2017-11-03 11:20:30 -0400

    improve for loop

--- src/LogicTier.java
+++ src/LogicTier.java
@@ -8,40 +8,55 @@ import java.util.List;
  */
 
 public class LogicTier {
-	
+
 	private DataTier dataTier; // link to the Data Tier
-	
+
 	public LogicTier(DataTier dataTier) {
 		this.dataTier = dataTier;
 	}
-	
+
 	public int findNumberOfBooksInYear(int year) {
 		List<Book> booksList = this.dataTier.getAllBooks();
-		//int countBooksInYear = 0;
-		//for (Book book: booksList) {
-			//if (book.getPublicationYear() == year) {
-				//countBooksInYear++;
-			//}
-		//}
-		//System.out.println("foreach count "+ countBooksInYear);
+		if (year <= 0) {
+			System.out.println("Returning 0 because year input is less or equals to 0");
+			return 0;
+		}
+		// int countBooksInYear = 0;
+		// for (Book book: booksList) {
+		// if (book.getPublicationYear() == year) {
+		// countBooksInYear++;
+		// }
+		// }
+		// System.out.println("foreach count "+ countBooksInYear);
 		int countStream = (int) booksList.stream().filter(x -> x.getPublicationYear() == year).count();
-		System.out.println("Stream count " + countStream);
+		//System.out.println("Stream count " + countStream);
 		return countStream;
 	}
-	
-	public ArrayList<String>  findBookTitlesByAuthor(String authorName){
+
+	public ArrayList<String> findBookTitlesByAuthor(String authorName) {
 		ArrayList<String> listOfTitles = new ArrayList<>();
-		System.out.println("look for " + authorName);
-		//*
-		  for (Book book:  this.dataTier.getAllBooks()) {
-			  System.out.println("Compare " + book.getAuthor() +" to " + authorName.replaceAll("\"", "").toLowerCase());
-			if (book.getAuthor().toLowerCase().equals(authorName.replaceAll("\"", "").toLowerCase())|| book.getAuthor().toLowerCase().contains(authorName.replaceAll("\"", "").toLowerCase()) ){
-				System.out.println("add" + book.getAuthor());
-				listOfTitles.add(book.getTitle());
-			}
-		}//*/
-		//this.dataTier.getAllBooks().stream().filter(x -> x.getAuthor().toLowerCase() == authorName.toLowerCase()).forEach(x-> listOfTitles.add(x.getAuthor()));
-		  System.out.println(listOfTitles.size());
+		if (authorName.equals(null) || authorName.isEmpty()) {
+			return listOfTitles;
+		}
+		/*
+		 * if (!authorName.equals(null)){
+		 * 
+		 * String authorNameToLowerCase = authorName.replaceAll("\"",
+		 * "").toLowerCase(); for (Book book: this.dataTier.getAllBooks()) {
+		 * final String currentAuthorToLowerCase =
+		 * book.getAuthor().toLowerCase(); if
+		 * (currentAuthorToLowerCase.equals(authorNameToLowerCase)||
+		 * currentAuthorToLowerCase.contains(authorNameToLowerCase) ){
+		 * System.out.println("add" + book.getAuthor());
+		 * listOfTitles.add(book.getTitle()); } } }
+		 */
+		// *
+		String authorNameToLowerCase = authorName.replaceAll("\"", "").toLowerCase();
+		this.dataTier.getAllBooks().stream()
+				.filter(x -> x.getAuthor().toLowerCase().equals(authorNameToLowerCase)
+						|| x.getAuthor().toLowerCase().contains(authorNameToLowerCase))
+				.forEach(x -> listOfTitles.add(x.getTitle()));
+		// */
 		return listOfTitles;
 	}
 }
--- src/PresentationTier.java
+++ src/PresentationTier.java
@@ -8,23 +8,24 @@ import java.util.Scanner;
  */
 
 public class PresentationTier {
-	
+
 	private LogicTier logicTier; // link to the Logic Tier
 	private Scanner scanner = new Scanner(System.in);
+
 	public PresentationTier(LogicTier logicTier) {
 		this.logicTier = logicTier;
 	}
-	
+
 	public void start() {
-		
+
 		int userOption = 0;
-		do{
+		do {
 			System.out.println("Choose a method");
-		System.out.println("1. Show Number Of Books In Year");
-		System.out.println("2. Show Book Titles By Author");
-		System.out.println("Enter the number option:");
-		userOption = scanner.nextInt();
-		}while(userOption <= 0 || userOption > 2);
+			System.out.println("1. Show Number Of Books In Year");
+			System.out.println("2. Show Book Titles By Author");
+			System.out.println("Enter the number option:");
+			userOption = scanner.nextInt();
+		} while (userOption <= 0 || userOption > 2);
 		switch (userOption) {
 		case 1:
 			showNumberOfBooksInYear();
@@ -35,30 +36,28 @@ public class PresentationTier {
 		default:
 			break;
 		}
-		
-		
-		
+
 	}
-	
+
 	public void showNumberOfBooksInYear() {
 		int year = 0;
-		do{ 
+		do {
 			System.out.println("Enter a year:");
 			year = scanner.nextInt();
-		}while(year==0);
-		
+		} while (year == 0);
+
 		System.out.println(this.logicTier.findNumberOfBooksInYear(year));
-		
+
 	}
-	
-	public void showBookTitlesByAuthor(){
+
+	public void showBookTitlesByAuthor() {
 		String authorName = "";
-		do{ 
+		do {
 			System.out.println("Enter author name:");
 			authorName = scanner.nextLine();
-		}while(authorName.length()==0);
-		
-		this.logicTier.findBookTitlesByAuthor(authorName).stream().forEach(System.out::println);;
+		} while (authorName.length() == 0);
+
+		this.logicTier.findBookTitlesByAuthor(authorName).stream().forEach(System.out::println);
 	}
 
 }
